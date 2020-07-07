ee0e73c360ba2a29a44636e3babecd263b12267f
Kristian Portillo <portillk@gmail.com>
2020-07-07 08:07:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_concat(my_favorite_things,more_favs)
   my_favorite_things.concat(more_favs)
 end
 
-def using_insert(list_of_programming_languages,another_languagee)
+def using_insert(list_of_programming_languages,another_language)
  new_array = list_of_programming_languages.insert(another_language)
 end
 
